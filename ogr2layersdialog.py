e2173110d9a374e705e10d124e9a4317c9b79170
Luca Delucchi <lucadeluge@gmail.com>
2012-09-01 16:29:02 +0200

    fix layout for mac platform

--- ogr2layersdialog.py
+++ ogr2layersdialog.py
@@ -1,9 +1,16 @@
 from PyQt4 import QtCore, QtGui 
-from ui_OGR2Layers import Ui_OGR2Layers
+import platform
+
 
 class OGR2LayersDialog(QtGui.QDialog): 
     def __init__(self): 
         QtGui.QDialog.__init__(self) 
         # Set up the user interface from Designer. 
-        self.ui = Ui_OGR2Layers() 
-        self.ui.setupUi(self)
+        if platform.system() == 'Darwin':
+            from ui_OGR2Layers_mac import Ui_OGR2Layers
+            self.ui = Ui_OGR2Layers() 
+            self.ui.setupUi(self)
+        else:
+            from ui_OGR2Layers import Ui_OGR2Layers
+            self.ui = Ui_OGR2Layers() 
+            self.ui.setupUi(self)
